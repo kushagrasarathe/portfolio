01a3afb67c8ea1ac85974813f146a75ff5ac0397
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-07-13 23:45:59 +0530

    basic landing page done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from '../../styles/Navbar.module.css'
+import Link from 'next/link'
 
-export default function () {
+export default function Navbar() {
   return (
     <div>
         <nav className={styles.nav}>
             <ul>
-                <a href='/'><h2>Kushagra Sarathe</h2></a>
+                <Link href='/'><h2>Kushagra Sarathe</h2></Link>
             </ul>
         </nav>
     </div>
